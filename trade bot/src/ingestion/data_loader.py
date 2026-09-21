821d9fe51072a19ff3dc07827cc1f0c3dd6d8acc
taylorknoles <knolestaylor@gmail.com>
2026-09-21 11:25:22 -0600

    Refactoring Alpaca_client with _request func

--- trade bot/src/ingestion/data_loader.py
+++ trade bot/src/ingestion/data_loader.py
@@ -1,11 +1,11 @@
 # Historical data loader
-
+import os
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockLatestQuoteRequest
 from utils.config_loader import logger
 
-api_key = "PKOZXTHYCD7VTBXATV5WOBVDCB"
-api_secret = "8MMU6kSyGfebWUYmZYKNQkegNfPhMqRS1QkPBTh7JoQ6"
+api_key = str(os.getenv("API_KEY"))
+api_secret = str(os.getenv("API_SECRET"))
 
 client = StockHistoricalDataClient(api_key, api_secret)
 
